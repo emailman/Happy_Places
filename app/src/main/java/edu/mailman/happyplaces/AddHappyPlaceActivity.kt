fc79bbaa27d04f7b0fb57f6c9680c11d5c4efe8b
Eric Mailman <emailman@dtcc.edu>
2023-01-06 10:58:31 -0500

    Setup Add Place Activity

--- app/src/main/java/edu/mailman/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/edu/mailman/happyplaces/AddHappyPlaceActivity.kt
@@ -2,10 +2,28 @@ package edu.mailman.happyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import edu.mailman.happyplaces.databinding.ActivityAddHappyPlaceBinding
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+
+    private var binding: ActivityAddHappyPlaceBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding?.toolbarAddPlace?.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
--- app/src/main/java/edu/mailman/happyplaces/MainActivity.kt
+++ app/src/main/java/edu/mailman/happyplaces/MainActivity.kt
@@ -1,11 +1,30 @@
 package edu.mailman.happyplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import edu.mailman.happyplaces.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.fabAddHappyPlace?.setOnClickListener {
+            val intent =
+                Intent(this@MainActivity, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+
 }
