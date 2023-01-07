3b19c4c43ff96420fa69d0705946b7465e75c75b
Eric Mailman <emailman@dtcc.edu>
2023-01-07 16:48:51 -0500

    Add code to set read permission

--- app/src/main/java/edu/mailman/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/edu/mailman/happyplaces/AddHappyPlaceActivity.kt
@@ -1,9 +1,21 @@
 package edu.mailman.happyplaces
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import edu.mailman.happyplaces.databinding.ActivityAddHappyPlaceBinding
 import java.text.SimpleDateFormat
 import java.util.*
@@ -33,11 +45,14 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
             cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
             updateDateInView()
         }
+
         binding?.etDate?.setOnClickListener(this)
+        binding?.tvAddImage?.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
         when (view!!.id) {
+
             R.id.et_date -> {
                 DatePickerDialog(this@AddHappyPlaceActivity, dateSetListener,
                     cal.get(Calendar.YEAR),
@@ -45,7 +60,61 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                     cal.get(Calendar.DAY_OF_MONTH)
                 ).show()
             }
+            R.id.tv_add_image -> {
+                Log.i("happy place image click", "")
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItems = arrayOf(
+                    "Select photo from gallery",
+                    "Capture photo from camera"
+                )
+                pictureDialog.setItems(pictureDialogItems) {
+                        _, which ->
+                    when (which) {
+                        0 -> choosePhotoFromGallery()
+                        1 -> Toast.makeText(this@AddHappyPlaceActivity,
+                            "photo from camera coming soon", Toast.LENGTH_LONG).show()
+                    }
+                }
+                pictureDialog.show()
+            }
         }
+    }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withContext(baseContext).withPermissions(
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        ).withListener(object: MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()) {
+                    Toast.makeText(this@AddHappyPlaceActivity,
+                        "Storage permission granted", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                permissions: MutableList<PermissionRequest>, token: PermissionToken) {
+                showRationalDialogForPermissions()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this).setMessage("Permissions not requested").
+        setPositiveButton("GO TO SETTINGS") {
+            _, _ ->
+            try {
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+            }
+        }.setNegativeButton("Cancel") {
+                dialog, _ ->
+            dialog.dismiss()
+        }.show()
     }
 
     private fun updateDateInView() {
